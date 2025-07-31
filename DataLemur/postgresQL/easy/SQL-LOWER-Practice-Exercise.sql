aca51bde0bb81a23503fd89e98ec8a344e5e9378
dzulfikar2403 <dzulfikar2403@gmail.com>
2025-07-31 23:08:04 +0700

    feat: day-55

---
+++ DataLemur/postgresQL/easy/SQL-LOWER-Practice-Exercise.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM customers 
+where lower(customer_name) like '%son' and age = 20;
+-- sesuai intruksi. kalo real case mending make ilike
+-- link soal: https://datalemur.com/questions/sql-lower-practice-exercise
