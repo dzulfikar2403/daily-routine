a6e3e13f04820dd92e15841eb0f613feb3d09ff1
dzulfikar2403 <dzulfikar2403@gmail.com>
2025-09-16 23:52:00 +0700

    feat: day-102

---
+++ pgexercises/postgresQL/basic/Control-which-rows-are-retrieved.sql
@@ -0,0 +1,4 @@
+select *
+from cd.facilities
+where membercost > 0;
+-- link soal: https://pgexercises.com/questions/basic/where.html
---
+++ pgexercises/postgresQL/basic/Retrieve-specific-columns-from-a-table.sql
@@ -0,0 +1,4 @@
+select name,
+		membercost
+from cd.facilities;
+-- link soal: https://pgexercises.com/questions/basic/selectspecific.html
